4fb92bfda56f3aca1020d84f56e6b9c1b348f013
jolly-sy <53500061+jolly-sy@users.noreply.github.com>
2019-08-10 08:46:17 +0800

    Update net_aton.c

--- net_aton.c
+++ net_aton.c
@@ -6,8 +6,8 @@ int main(int argc,char **argv)
 	int ret;
 	struct in_addr addr;
 	ret=inet_aton(argv[1],&addr);
-	printf("aton():%x\n",addr.s_addr);
-	printf("inet%x\n",inet_addr(argv[1]));
+	printf("aton():%x\n",addr.s_addr);//转化成网络字节序形式
+	printf("inet%x\n",inet_addr(argv[1]));//转化回点分十进制形式
 	
 	return 0;
 }
